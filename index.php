e53a18e0cce3e98c77c30c19c4ff98a3211341a4
asaadefrei <130939504+asaadefrei@users.noreply.github.com>
2023-04-17 01:27:02 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+session_start();
+
+
+
+include ("views/header.php");
+include("models/inscription.class.php");
+
+include("models/eleve.class.php");
+include("models/seance.class.php");
+include("models/instrument.class.php");
+
+
+include("models/prof.class.php");
+include("models/admin.class.php");
+include("models/monPdo.php");
+
+
+if(empty($_GET["uc"])){
+  $uc="connection";
+
+}else{
+  $uc=$_GET["uc"];
+
+}
+
+
+
+switch($uc){
+  case "connection":
+    include "views/connection.php";
+      break;
+    case "admin":
+      include("controleurs/controleurAdmin.php");
+      break;
+    case "accueil":
+      include("controleurs/controleurAdmin.php");
+      break;
+    case "prof":
+      include("controleurs/controleurAdmin.php");
+      break;
+    case "cours":
+      include("controleurs/controleurAdmin.php");
+       break;
+    case "eleve":
+      include("controleurs/controleurAdmin.php");
+       break;
+    
+    
+
+
+  
+
+
+}
+
+
+include "views/footer.php";
+
+
+
+
+?>
